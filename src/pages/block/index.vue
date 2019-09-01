5ccee42992ac85231efc47795dbc77e4171dead9
gexiaolin <282274241@qq.com>
2019-09-01 12:50:43 +0800

    add block project

---
+++ src/pages/block/index.vue
@@ -0,0 +1,43 @@
+<template>
+    <div class="main">
+        <canvas id="block" ref="canvas"></canvas>
+    </div>
+</template>
+
+<script>
+    import Ctx from 'src/assets/ctx';
+
+    export default {
+        computed: {
+            ctx () {
+                return Ctx(this.$refs.canvas, '2d');
+            }
+        },
+        mounted () {
+            this.$nextTick(() => {
+                this.init();
+            });
+        },
+        methods: {
+            init () {
+                this.resetSize();
+            },
+            resetSize () {
+                let ctx = this.ctx;
+                let winWidth = this.winWidth = window.innerWidth;
+                let winHeight = this.winHeight = window.innerHeight;
+                this.$refs.canvas.width = winWidth;
+                this.$refs.canvas.height = winHeight;
+                
+                ctx.clearRect(0, 0, this.winWidth, this.winHeight);
+
+                ctx.fillStyle = '#eee';
+                ctx.fillRect(0, 0, this.winWidth, this.winHeight);
+            },
+        }
+    };
+</script>
+
+<style lang="scss">
+    @import 'src/assets/base';
+</style>
