f617179658aa2166b873376a9c2d42b5f1118972
gexiaolin <282274241@qq.com>
2019-09-05 16:10:48 +0800

    add flame & init

---
+++ src/pages/flame/flame.vue
@@ -0,0 +1,44 @@
+<template>
+    <div class="main">
+        <canvas id="flame" ref="canvas"></canvas>
+    </div>
+</template>
+
+<script>
+    import 'raf.js';
+    import Ctx from 'src/assets/ctx';
+
+    export default {
+        data () {
+            return {
+
+            };
+        },
+        computed: {
+            ctx () {
+                return Ctx(this.$refs.canvas, '2d');
+            }
+        },
+        mounted () {
+            this.$nextTick(() => {
+                this.init();
+            });
+        },
+        methods: {
+            init () {
+                this.resetSize();
+            },
+            resetSize () {
+                let ctx = this.ctx;
+                let winWidth = this.winWidth = window.innerWidth;
+                let winHeight = this.winHeight = window.innerHeight;
+                this.$refs.canvas.width = winWidth;
+                this.$refs.canvas.height = winHeight;
+            }
+        }
+    };
+</script>
+
+<style lang="scss">
+    @import 'src/assets/base';
+</style>
